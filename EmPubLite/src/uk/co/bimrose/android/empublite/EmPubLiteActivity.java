b1d53243b913ae44096bdd31cfcc7b7284f03fa4
BlueJam <bimrose@gmail.com>
2013-10-29 15:52:00 +0000

    Initialisation of the project. I've only just started using Git, so this project has already been worked on a little.

---
+++ EmPubLite/src/uk/co/bimrose/android/empublite/EmPubLiteActivity.java
@@ -0,0 +1,15 @@
+package uk.co.bimrose.android.empublite;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class EmPubLiteActivity extends SherlockFragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+}
