edf9e0e48c4df39eb7e7d7d4b9dc1090e60cba8f
BlueJam <bimrose@gmail.com>
2013-11-11 15:37:56 +0000

    Adding the book content, putting it in a pager, plus a few other things.

--- EmPubLite/src/uk/co/bimrose/android/empublite/EmPubLiteActivity.java
+++ EmPubLite/src/uk/co/bimrose/android/empublite/EmPubLiteActivity.java
@@ -2,6 +2,8 @@ package uk.co.bimrose.android.empublite;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
@@ -10,35 +12,58 @@ import com.actionbarsherlock.view.MenuItem;
 
 public class EmPubLiteActivity extends SherlockFragmentActivity {
 
+	private ViewPager pager = null;
+	private ContentsAdapter adapter = null;
+	private static final String MODEL = "model";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (getSupportFragmentManager().findFragmentByTag(MODEL) == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(new ModelFragment(), MODEL).commit();
+		}
 		setContentView(R.layout.main);
+		pager = (ViewPager) findViewById(R.id.pager);
+		
+		getSupportActionBar().setHomeButtonEnabled(true);
+	}
+
+	void setupPager(BookContents contents) {
+		adapter = new ContentsAdapter(this, contents);
+		pager.setAdapter(adapter);
+		findViewById(R.id.progressBar1).setVisibility(View.GONE);
+		findViewById(R.id.pager).setVisibility(View.VISIBLE);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		new MenuInflater(this).inflate(R.menu.options, menu);
-		return(super.onCreateOptionsMenu(menu));
+		return (super.onCreateOptionsMenu(menu));
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()){
+		switch (item.getItemId()) {
 		case android.R.id.home:
-			return(true);
-			
+			pager.setCurrentItem(0, false);
+			return (true);
+
 		case R.id.about:
 			Intent i = new Intent(this, SimpleContentActivity.class);
+			i.putExtra(SimpleContentActivity.EXTRA_FILE,
+					"file:///android_asset/misc/about.html");
 			startActivity(i);
-			return(true);
-			
+			return (true);
+
 		case R.id.help:
 			i = new Intent(this, SimpleContentActivity.class);
+			i.putExtra(SimpleContentActivity.EXTRA_FILE,
+					"file:///android_asset/misc/help.html");
 			startActivity(i);
-			return(true);
+			return (true);
 		}
-		return(super.onOptionsItemSelected(item));
+		return (super.onOptionsItemSelected(item));
 	}
 
 }
